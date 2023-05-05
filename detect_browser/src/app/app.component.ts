4d4940b66e8caefb469245e59eca71d96ac2de8a
SEANGLAY <liseanglay@gmail.com>
2023-05-05 14:11:26 +0700

    completed dective

--- detect_browser/src/app/app.component.ts
+++ detect_browser/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'detect_browser';
+  browserName = '';
+  browserVersion = '';
+
+  ngOnInit() {
+    this.browserName = this.detectBrowserName();
+    this.browserVersion = this.detectBrowserVersion();
+}
+  detectBrowserName() { 
+    const agent = window.navigator.userAgent.toLowerCase()
+    switch (true) {
+      case agent.indexOf('edge') > -1:
+        return 'edge';
+      case agent.indexOf('opr') > -1 && !!(<any>window).opr:
+        return 'opera';
+      case agent.indexOf('chrome') > -1 && !!(<any>window).chrome:
+        return 'chrome';
+      case agent.indexOf('trident') > -1:
+        return 'ie';
+      case agent.indexOf('firefox') > -1:
+        return 'firefox';
+      case agent.indexOf('safari') > -1:
+        return 'safari';
+      default:
+        return 'other';
+    }
+  }
+  detectBrowserVersion(){
+    var userAgent = navigator.userAgent, tem, 
+    matchTest = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    
+    if(/trident/i.test(matchTest[1])){
+        tem =  /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(matchTest[1]=== 'Chrome'){
+        tem = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    matchTest= matchTest[2]? [matchTest[1], matchTest[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= userAgent.match(/version\/(\d+)/i))!= null) matchTest.splice(1, 1, tem[1]);
+    return matchTest.join(' ');
+}
+
+  
+
 }
